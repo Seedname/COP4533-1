ec2665639ebb16421026ee2f2bc67c9ab9ac510a
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2026-01-27 11:48:08 -0500

    create input reader

---
+++ src/verifier.py
@@ -0,0 +1,11 @@
+from matcher import read_input
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    hospitals, students, n = read_input("example")
+
+    print(hospitals)
+    print(students)
+    print(n)
