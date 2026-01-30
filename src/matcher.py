10f791229d4e8cefcbd692d11dc8694852195e8d
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2026-01-30 11:41:52 -0500

    finish the analyzer

--- src/matcher.py
+++ src/matcher.py
@@ -16,9 +16,6 @@ def matcher(hospitals: list[list[int]], students: list[list[int]], n: int):
         for j in range(n):
             hospital = students[i][j]-1
             student_preference_maps[-1][hospital] = j
-            
-    print(student_preference_maps)
-    
     
     # count the number of consecutive checks to know when to stop
     count_checks = 0
@@ -60,9 +57,6 @@ def matcher(hospitals: list[list[int]], students: list[list[int]], n: int):
         matches.append((i+1, hospital_matches[i]+1))
         
     return matches
-    
-if __name__ == "__main__":
-    main()
 
 
 def read_input(file_name: str) -> tuple[list[list[int]], list[list[int]], int]:
@@ -123,6 +117,5 @@ def read_input(file_name: str) -> tuple[list[list[int]], list[list[int]], int]:
 if __name__ == "__main__":
     hospitals, students, n = read_input("example")
 
-    print(hospitals)
-    print(students)
-    print(n)
+    matcher_output = matcher(hospitals, students, n)
+    print(matcher_output)
